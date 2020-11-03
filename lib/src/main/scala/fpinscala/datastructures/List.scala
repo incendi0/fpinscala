27e4ffbf94a10a913c33c1b4f1d458906bd7e014
incendi0 <1184813573@qq.com>
2020-11-03 23:09:17 +0800

    List小试牛刀

--- lib/src/main/scala/fpinscala/datastructures/List.scala
+++ lib/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
@@ -8,5 +10,38 @@ object List {
   def apply[A](as: A*): List[A] = {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
+  }
+
+  def tail[A](as: List[A]): List[A] = as match {
+    case Nil => sys.error("tail on empty list")
+    case Cons(_, tail) => tail
+  }
+
+  def setHead[A, B >: A](a: A, bs: List[B]): List[B] = bs match {
+    case Nil => sys.error("setHead on empty list")
+    case Cons(_, tail) => Cons(a, tail)
+  }
+
+  @tailrec
+  def drop[A](as: List[A], n: Int): List[A] = as match {
+    case Cons(_, tail) if n > 0 => drop(tail, n - 1)
+    case _ => as
+  }
+
+  @tailrec
+  def dropWhile[A](as: List[A])(f: A => Boolean): List[A] = as match {
+    case Cons(head, tail) if f(head) => dropWhile(tail)(f)
+    case _ => as
+  }
+
+  def append[A](as1: List[A], as2: List[A]): List[A] = as1 match {
+    case Cons(head, tail) => Cons(head, append(tail, as2))
+    case _ => as2
+  }
+
+  def init[A](as: List[A]): List[A] = as match {
+    case Nil => sys.error("init on empty list")
+    case Cons(_, Nil) => Nil
+    case Cons(head, tail) => Cons(head, init(tail))
   }
 }
--- lib/src/test/scala/fpinscala/datastructures/ListSuite.scala
+++ lib/src/test/scala/fpinscala/datastructures/ListSuite.scala
@@ -2,6 +2,8 @@ package fpinscala.datastructures
 
 import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.have
+import org.scalatest.matchers.should.Matchers.{convertToAnyShouldWrapper, the}
 import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
@@ -10,4 +12,22 @@ class ListSuite extends AnyFunSuite {
     val oneTwoThree = Cons(1, Cons(2, Cons(3, Nil)))
     assert(oneTwoThree == List.apply(1, 2, 3))
   }
+
+  test("tail abandons the first element") {
+    val oneTwoThree = Cons(1, Cons(2, Cons(3, Nil)))
+    assert(List.tail(oneTwoThree) == Cons(2, Cons(3, Nil)))
+  }
+
+  the[RuntimeException] thrownBy {
+    List.tail(Nil)
+  } should have message "tail on empty list"
+
+  test("setHead replace the first element") {
+    val oneTwoThree = Cons(1, Cons(2, Cons(3, Nil)))
+    assert(List.setHead(0, oneTwoThree) == Cons(0, Cons(2, Cons(3, Nil))))
+  }
+
+  the[RuntimeException] thrownBy {
+    List.setHead(1, Nil)
+  } should have message "setHead on empty list"
 }
