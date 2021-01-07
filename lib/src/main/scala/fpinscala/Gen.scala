39c7ee43ab7319e77f1edbae16edc1027d33f58f
incendi0 <69083424+incendi0@users.noreply.github.com>
2021-01-07 17:34:07 +0800

    Create Gen.scala

---
+++ lib/src/main/scala/fpinscala/Gen.scala
@@ -0,0 +1,152 @@
+package fpinscala.testing
+
+import fpinscala.state.{RNG, State}
+import fpinscala.testing.Prop.{Falsified, MaxSize, Passed, Proved, Result, TestCases, forAll}
+
+case class Gen[+A](sample: State[RNG, A]) {
+  def map[B](f: A => B): Gen[B] = Gen(sample.map(f))
+
+  def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(a => f(a).sample))
+
+  def map2[B, C](g: Gen[B])(f: (A, B) => C): Gen[C] =
+    for {
+      a <- this
+      b <- g
+    } yield f(a, b)
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(n => Gen.listOfN(n, this))
+
+  def unsized: SGen[A] = SGen(_ => this)
+
+  def **[B](g: Gen[B]): Gen[(A, B)] = this.map2(g)((_, _))
+
+}
+
+object Gen {
+  def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
+
+  def boolean: Gen[Boolean] = Gen(State(RNG.boolean))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = Gen(State.sequence(List.fill(n)(g.sample)))
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap(b => if (b) g1 else g2)
+
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+    val g1Threshold = g1._2 / (g1._2 + g2._2)
+    Gen(State(RNG.doubleRNG).flatMap(d => if (d < g1Threshold) g1._1.sample else g2._1.sample))
+  }
+
+  def listOf[A](g: Gen[A]): SGen[List[A]] = SGen(n => Gen.listOfN(n, g))
+
+  def listOf1[A](g: Gen[A]): SGen[List[A]] = SGen(n => Gen.listOfN(n max 1, g))
+
+  def uniform: Gen[Double] = Gen(State(RNG.doubleRNG))
+
+  def choose(l: Double, r: Double): Gen[Double] =
+    Gen(State(RNG.doubleRNG).map(d => l + d * (r - l)))
+
+  val smallInt: Gen[Double] = Gen.choose(-10,10)
+  val maxProp: Prop = forAll(listOf(smallInt)) { l =>
+    val max = l.max
+    !l.exists(_ > max)
+  }
+
+  val sortedProp: Prop = forAll(listOf(smallInt)) { ds =>
+    val sds = ds.sorted
+    sds.isEmpty || sds.tail.isEmpty || !sds.zip(sds.tail).exists {case (a, b) => a > b }
+  }
+
+  // test Par
+}
+
+case class SGen[+A](g: Int => Gen[A]) {
+  def apply(n: Int): Gen[A] = g(n)
+
+  def map[B](f: A => B): SGen[B] =
+    SGen { n => g(n).map(f) }
+
+  def flatMap[B](f: A => SGen[B]): SGen[B] = {
+    SGen { n => g(n).flatMap(a => f(a).g(n))}
+  }
+
+  def **[B](s2: SGen[B]): SGen[(A,B)] =
+    SGen(n => apply(n) ** s2(n))
+}
+
+case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
+  def &&(p: Prop): Prop = Prop {
+    (max, n, rng) => run(max, n, rng) match {
+      case Passed | Proved => p.run(max, n, rng)
+      case x => x
+    }
+  }
+
+  def ||(p: Prop): Prop = Prop {
+    (max, n, rng) => run(max, n, rng) match {
+      case Falsified(msg, _) => p.tag(msg).run(max, n, rng)
+      case x => x
+    }
+  }
+
+  def tag(msg: String): Prop = Prop {
+    (max, n, rng) => run(max, n, rng) match {
+      case Falsified(e, c) => Falsified(msg + "\n" + e, c)
+      case x => x
+    }
+  }
+}
+
+object Prop {
+  type SuccessCount = Int
+  type TestCases = Int
+  type MaxSize = Int
+  type FailedCase = String
+
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+
+  case object Passed extends Result {
+    override def isFalsified: Boolean = false
+  }
+
+  case class Falsified(failure: FailedCase, Successes: SuccessCount) extends Result {
+    override def isFalsified: Boolean = true
+  }
+
+  case object Proved extends Result {
+    override def isFalsified: Boolean = false
+  }
+
+  def randomStream[A](g: Gen[A])(rng: RNG): LazyList[A] =
+    LazyList.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n,rng) => randomStream(as)(rng).zip(LazyList.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  def apply(f: (TestCases, RNG) => Result): Prop =
+    Prop { (_, n, rng) => f(n,rng) }
+
+  def forAll[A](g: SGen[A])(f: A =>Boolean): Prop = forAll(g(_))(f)
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max, n, rng) =>
+      val casePerSize = (n - 1) / max + 1
+      val props: LazyList[Prop] = LazyList.from(0).take((n min max) + 1).map(i => forAll(g(i))(f))
+      val prop: Prop = props.map(p => Prop { (max, n, rng) => p.run(max, casePerSize, rng)}).toList.reduce(_ && _)
+      prop.run(max, n, rng)
+  }
+}
+
+
+
